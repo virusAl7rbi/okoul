2a6ae278c92235e55add577264efa46f408fd48f
virusal7rbi <fahadalwasidi@gmail.com>
2022-09-21 14:30:45 +0300

    remove temp

--- QuizU/flutter_application_1/android/app/src/main/kotlin/com/example/flutter_application_1/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.viros.okoul.quizu
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- QuizU/flutter_application_1/lib/pages.dart
+++
@@ -1,4 +0,0 @@
-import 'package:flutter_application_1/leadboardpage.dart';
-import 'package:flutter_application_1/homepage.dart';
-import 'package:flutter_application_1/profilepage.dart';
-import 'package:flutter_application_1/loginpage.dart';
